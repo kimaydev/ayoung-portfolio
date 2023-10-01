5beb5abc3549d0598fbbe7f3a4a23d860ffeac47
kimaydev <kimaydev@gmail.com>
2023-10-01 14:19:00 +0900

    feat: 화면에 출력할 값 정리 작업 완료

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { postData } from '@/lib/patchData';
 import ListItem from '@/components/portfolio/ListItem';
 
-const Frontend = ({ resultId }: any) => {
+export interface resultType {
+  resultId: string;
+  resultThum: string;
+}
+
+const Frontend = ({ resultId, resultThum }: resultType) => {
   // console.log(resultId);
   return (
     <div className="max-w-screen-lg mx-auto">
@@ -19,8 +24,24 @@ const Frontend = ({ resultId }: any) => {
 export default Frontend;
 
 export const getStaticProps = async () => {
+  // 전달받은 데이터
   const resultData = await postData();
+  // id
   const resultId = resultData.results.map((item: any) => item.id);
+  // 노션 상세페이지 주소
+  const resultUrl = resultData.results.map((item: any) => item.public_url);
+  // properties (화면에 출력할 값)
+  const properties = resultData.results.map((item: any) => item.properties);
+  // 썸네일 이미지
+  const resultThum = properties.map((item: any) => item.thum.files[0].file.url);
+  // 프로젝트 기간
+  const resultDate = properties.map((item: any) => item.date);
+  // 타이틀
+  const resultTitle = properties.map(
+    (item: any) => item.이름.title[0].plain_text,
+  );
+
+  // console.log(`결과 출력 ${resultUrl}`);
 
   return { props: { resultId } };
 };
